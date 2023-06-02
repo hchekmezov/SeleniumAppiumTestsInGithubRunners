38ee98db0d7fb915194f69322218c8dbe8aecf3d
hchekmezov <hchekmezov@solvd.com>
2023-06-02 11:58:00 +0300

    added udid for ios tests

--- src/test/java/com/mobile/IosTest.java
+++ src/test/java/com/mobile/IosTest.java
@@ -19,7 +19,8 @@ public class IosTest {
         capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, Platform.IOS);
         capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "XCUITest");
         capabilities.setCapability("isHeadless",true);
-        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPod touch (7th generation)");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "IPhone");
+        capabilities.setCapability(MobileCapabilityType.UDID, "9A856D0A-3E48-4611-9E9C-21BF9C1B1FA6");
         //capabilities.setCapability(MobileCapabilityType.APP, System.getProperty("user.dir")+"/DailyCheck.zip");
         capabilities.setCapability(MobileCapabilityType.APP, System.getProperty("user.dir")+"/TestApp-iphonesimulator.app");
         AndroidDriver<AndroidElement> driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"),
